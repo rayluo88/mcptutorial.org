f0b96ea8165672ba251e0a585bf3eee8eb4cfd55
rayluo88 <raymond.luo0902@gmail.com>
2025-03-30 21:59:13 +0800

    path aliases in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // swcMinify is now the default in Next.js 13+
   // experimental.appDir is no longer needed as it's the default in Next.js 14+
+  webpack: (config) => {
+    config.resolve.alias['#'] = path.join(__dirname, '.');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
